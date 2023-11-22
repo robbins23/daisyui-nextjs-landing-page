6585f66375472ef90766c66ee9019719d8e66517
Robbin Singh <srobbin01@gmail.com>
2023-11-22 21:29:18 +0530

    contactus changed

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,7 +4,7 @@ export default function Page() {
     return (
       <InnerPageContainer title="Contact Us">
             <p>
-            We are reachable at - info@landingpage.com
+            We are reachable at - info@webai.com
             </p>
             
       </InnerPageContainer>
